d9fa027e4e1f8cad9de27b9a3e92302ac5ee5e1a
Clemie McCartney <nmatthewsoft@gmail.com>
2023-01-12 12:20:59 +0700

    Bump discord.py v1.7.3 to v2.1.0 (#1)
    * merge cogs to main
    
    * update dpy to latest
    
    * minor typofix
    
    * using env instead type the id
    
    * build using actor username, instead org name
    
    * flyio configs init
    
    * deploy to fly.io
    
    * fix directory
    
    * add pre-commit checks
    
    * add FUNDING links
    
    * add pr template
    
    * pre-commit config init
    
    * fix: pre-commit auto fixes
    
    Co-authored-by: peradaban-bot[bot] <110201555+peradaban-bot[bot]@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,29 +1,113 @@
 import os
 import sys
-
 import discord
-import discord.utils
 import logging
+import datetime
+
 from dotenv import load_dotenv
-from discord import Webhook
 from discord.ext import commands
+from pymongo import MongoClient
 
 load_dotenv()
 intents = discord.Intents().all()
 intents.members = True
-client = commands.Bot(command_prefix=":", intents=intents)
-client.remove_command("help")
+client = discord.Client(intents=intents)
 
+cluster = MongoClient(os.getenv("MONGODB_URL"))
 
-for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
-        client.load_extension(f"cogs.{filename[:-3]}")
+levelling = cluster["dagelan"]["levelling"]
 
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.offline)
+    await client.change_presence(status=discord.Status.online)
     logging.info(f"Peradaban (Utilities) is ready for action!")
+
+
+@client.event
+async def on_user_update(before, after):
+    guild = client.get_guild(os.getenv("GUILD_ID"))
+    channel = guild.get_channel(os.getenv("CHANNEL_ID"))
+    if guild.get_member(before.id):
+        if not before.bot:
+            if before.display_avatar != after.display_avatar:
+                stats = levelling.find_one({"id": before.id})
+                if stats is not None:
+                    levelling.update_one(
+                        {"id": before.id},
+                        {"$set": {"image_url": str(after.display_avatar)}},
+                    )
+                embed = discord.Embed(
+                    description=f"{before.mention} **Avatar Changed**",
+                    colour=discord.Colour.blurple(),
+                )
+                embed.set_author(
+                    name=f"{before.name}#{before.discriminator}",
+                    icon_url=before.display_avatar,
+                    url=f"https://discord.com/users/{before.id}",
+                )
+                embed.set_thumbnail(url=after.display_avatar)
+                embed.add_field(
+                    name=f"Avatar",
+                    value=f"[`before`]({before.display_avatar}) -> [`after`]({after.display_avatar})",
+                    inline=True,
+                )
+                embed.set_footer(text=f"ID: {before.id}")
+                embed.timestamp = datetime.datetime.utcnow()
+                await channel.send(embed=embed)
+
+            if before.name != after.name:
+                stats = levelling.find_one({"id": before.id})
+                if stats is not None:
+                    levelling.update_one(
+                        {"id": before.id}, {"$set": {"username": after.name}}
+                    )
+                    if after.nick is None:
+                        levelling.update_one(
+                            {"id": after.id}, {"$set": {"displayname": after.name}}
+                        )
+                embed = discord.Embed(
+                    description=f"{before.mention} **Username Changed**",
+                    colour=discord.Colour.blurple(),
+                )
+                embed.set_author(
+                    name=f"{before.name}#{before.discriminator}",
+                    icon_url=after.display_avatar,
+                    url=f"https://discord.com/users/{before.id}",
+                )
+                embed.set_thumbnail(url=after.display_avatar)
+                embed.add_field(name=f"Before:", value=f"`{before.name}`", inline=True)
+                embed.add_field(name=f"After:", value=f"`{after.name}`", inline=True)
+                embed.set_footer(text=f"ID: {before.id}")
+                embed.timestamp = datetime.datetime.utcnow()
+                await channel.send(embed=embed)
+
+            if before.discriminator != after.discriminator:
+                stats = levelling.find_one({"id": before.id})
+                if stats is not None:
+                    levelling.update_one(
+                        {"id": before.id},
+                        {"$set": {"discrim": after.discriminator}},
+                    )
+                embed = discord.Embed(
+                    description=f"{before.mention} **Discriminator Changed**",
+                    colour=discord.Colour.blurple(),
+                )
+                embed.set_author(
+                    name=f"{before.name}#{before.discriminator}",
+                    icon_url=after.display_avatar,
+                    url=f"https://discord.com/users/{before.id}",
+                )
+                embed.set_thumbnail(url=after.display_avatar)
+                embed.add_field(
+                    name=f"Before:", value=f"`#{before.discriminator}`", inline=True
+                )
+                embed.add_field(
+                    name=f"After:", value=f"`#{after.discriminator}`", inline=True
+                )
+                embed.set_footer(text=f"ID: {before.id}")
+                embed.timestamp = datetime.datetime.utcnow()
+                await channel.send(embed=embed)
 
 
 client.run(os.getenv("BOT_TOKEN"))
